2a6583f2ac4ef880ec1d5ca5f9f318f9d78c6de7
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-21 09:10:15 +0300

    Create Controller

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+
+    }
+}
