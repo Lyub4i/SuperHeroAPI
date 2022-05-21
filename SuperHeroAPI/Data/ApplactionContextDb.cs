81ea4f8f5c6116b8111031257761b80da687fc07
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-21 18:47:48 +0300

    Create DbContext

---
+++ SuperHeroAPI/Data/ApplactionContextDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class ApplactionContextDb : DbContext
+    {
+        public ApplactionContextDb(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> Heroes { get; set; }
+    }
+}
