b655415af891c289478c7ec7d930ddc8832c8a32
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-21 14:31:09 +0300

    Correct HttpGet

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -7,13 +7,30 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : Controller
     {
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        private static List<SuperHero> heroes = new List<SuperHero>
         {
-            var heroes = new List<SuperHero>
+            new SuperHero
             {
-                new SuperHero{ }
+                Id =1,
+                Name = "Spider Man",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="New Yourk"
+            },
+            new SuperHero
+            {
+                Id =2,
+                Name = "Iron Man",
+                FirstName="Tony",
+                LastName="Stark",
+                Place="New York"
             }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
         }
     }
 }
